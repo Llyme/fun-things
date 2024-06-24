469d87ce1d10e7e76dab3cbfbcb862971e6262de
Michael Edmund Wong <infinitex777@gmail.com>
2024-06-24 13:50:12 +0800

    Added Categorizer.

--- fun_things/__init__.py
+++ fun_things/__init__.py
@@ -3,3 +3,4 @@ from .proxy_uri import ProxyURI
 from .type import get_all_descendant_classes
 from .retry.retry import Retry
 from .retry.retry_response import RetryResponse
+from .categorizer import categorizer
