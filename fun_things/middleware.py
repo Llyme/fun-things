76e7f6dd27e317586fc80e67dad9cdec9c9f8046
Michael Edmund Wong <infinitex777@gmail.com>
2024-09-03 09:40:22 +0800

    Improved middleware class.

--- fun_things/middleware.py
+++ fun_things/middleware.py
@@ -1,15 +1,38 @@
 from abc import ABC
+import asyncio
 from signal import SIGABRT, SIGCONT, SIGQUIT
-from typing import Generic, List, Type, TypeVar, Union, final
-from fun_things import as_asyncgen
+from typing import (
+    Dict,
+    Generic,
+    List,
+    Optional,
+    Type,
+    TypeVar,
+    Union,
+    final,
+)
+from fun_things import as_asyncgen, as_gen
 
-T = TypeVar("T", bound="Middleware")
-TRetrieve = TypeVar("TRetrieve")
+TParent = TypeVar("TParent", bound="Middleware")
+TChild = TypeVar("TChild", bound="Middleware")
 
 
-class Middleware(Generic[T], ABC):
-    middlewares: List[Type["Middleware"]] = []
-    parent: T = None  # type: ignore
+class Middleware(Generic[TParent], ABC):
+    PRIORITY: int = 0
+    MIDDLEWARES: List[Type["Middleware"]] = []
+    """
+    Nested middlewares.
+    """
+    parent: TParent = None  # type: ignore
+
+    __middleware_instances: Dict[
+        Type["Middleware"],
+        "Middleware",
+    ]
+    __all_middleware_instances: Dict[
+        Type["Middleware"],
+        "Middleware",
+    ]
 
     @property
     def root(self):
@@ -17,9 +40,9 @@ class Middleware(Generic[T], ABC):
 
     def get_middleware(
         self,
-        type: Union[Type[TRetrieve], str],
+        type: Union[Type[TChild], str],
         recursive: bool = True,
-    ) -> TRetrieve:
+    ) -> TChild:
         middlewares = self.__middleware_instances
 
         if recursive:
@@ -28,22 +51,77 @@ class Middleware(Generic[T], ABC):
         if isinstance(type, str):
             for key in middlewares:
                 if key.__name__ == type:
-                    return middlewares[key]
+                    return middlewares[key]  # type: ignore
 
             return None  # type: ignore
 
         return middlewares.get(type)  # type: ignore
 
-    async def before_run(self):
+    def before_run(self):
+        """
+        Called before the nested middlewares are called.
+
+        Can be asynchronous.
+        """
         pass
 
-    async def after_run(self):
+    def after_run(self):
+        """
+        Called after the nested middlewares are called.
+
+        Can be asynchronous.
+        """
         pass
 
     @final
-    async def run(self):
+    def __instantiate(self, middleware: Type[TChild]):
+        instance = middleware()
+        self.__middleware_instances[middleware] = instance
+        self.__root.__all_middleware_instances[middleware] = instance
+        instance.parent = self
+
+        return instance
+
+    @final
+    def __build_annotations(self):
+        if "__annotations__" not in self.__class__.__dict__:
+            return
+
+        annotations = self.__class__.__annotations__
+
+        for key, type in annotations.items():
+            if not issubclass(type, Middleware):
+                continue
+
+            setattr(self, key, self.__instantiate(type))
+
+    @final
+    def run_all(
+        self,
+        loop: Optional[asyncio.AbstractEventLoop] = None,
+    ):
+        return [*self.run(loop)]
+
+    @final
+    def run(
+        self,
+        loop: Optional[asyncio.AbstractEventLoop] = None,
+    ):
+        return as_gen(self.run_async(), loop)
+
+    @final
+    async def run_all_async(self):
+        results = []
+
+        async for result in self.run_async():
+            results.append(result)
+
+        return results
+
+    @final
+    async def run_async(self):
         if self.parent == None:
-            self.__all_middleware_instances: dict = {
+            self.__all_middleware_instances = {
                 self.__class__: self,
             }
             self.__root = self
@@ -52,48 +130,46 @@ class Middleware(Generic[T], ABC):
 
         self.__middleware_instances = {}
 
-        # print("START", self.__class__)
+        self.__build_annotations()
 
-        async for result in as_asyncgen(self.before_run()):  # type: ignore
+        for middleware in self.MIDDLEWARES:
+            self.__instantiate(middleware)
+
+        async for item in as_asyncgen(self.before_run()):
             # print("BEFORE RUN", self.__class__, result)
-            if result == None:
+
+            if item == SIGCONT:
                 continue
 
-            if result == SIGCONT:
-                continue
-
-            if result == SIGABRT:
+            if item == SIGABRT:
                 return
 
-            if result == SIGQUIT:
+            if item == SIGQUIT:
                 if self.parent != None:
                     yield SIGQUIT
 
                 return
 
-            yield result
+        # print("START", self.__class__)
 
+        middlewares = sorted(
+            self.__middleware_instances.values(),
+            key=lambda middleware: middleware.PRIORITY,
+            reverse=True,
+        )
         abort = False
 
-        for middleware in self.middlewares:
-            instance = middleware()
-            self.__middleware_instances[middleware] = instance
-            self.__root.__all_middleware_instances[middleware] = instance
-            instance.parent = self
-
-            async for result in as_asyncgen(instance.run()):
+        for middleware in middlewares:
+            async for item in as_asyncgen(middleware.run_async()):
                 # print("MIDDLEWARE", middleware, result)
-                if result == None:
+                if item == SIGCONT:
                     continue
 
-                if result == SIGCONT:
-                    continue
-
-                if result == SIGABRT:
+                if item == SIGABRT:
                     abort = True
                     break
 
-                if result == SIGQUIT:
+                if item == SIGQUIT:
                     abort = True
 
                     if self.parent != None:
@@ -101,27 +177,19 @@ class Middleware(Generic[T], ABC):
 
                     break
 
-                yield result
-
             if abort:
                 break
 
-        async for result in as_asyncgen(self.after_run()):  # type: ignore
+        async for item in as_asyncgen(self.after_run()):
             # print("AFTER RUN", self.__class__, result)
-
-            if result == None:
+            if item == SIGCONT:
                 continue
 
-            if result == SIGCONT:
-                continue
-
-            if result == SIGABRT:
+            if item == SIGABRT:
                 return
 
-            if result == SIGQUIT:
+            if item == SIGQUIT:
                 if self.parent != None:
                     yield SIGQUIT
 
                 return
-
-            yield result
