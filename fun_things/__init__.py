0b54f58cdc1721451177c90d8c907964c26f598d
Michael Edmund Wong <infinitex777@gmail.com>
2025-01-30 17:22:47 +0800

    Added network.

--- fun_things/__init__.py
+++ fun_things/__init__.py
@@ -16,3 +16,4 @@ from .url import re_escape_special_chars, re_escape_url
 from .ad_blocker_filter_parser import AdBlockerFilterParser, AdBlockerFilterData
 from .generic import merge_dict
 from .singleton_factory import SingletonFactory
+from .network import ping
