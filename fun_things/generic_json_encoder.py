b4de4e17307de1f2eb8cccd996523be274311815
Llyme <michael.edmund.wong@gmail.com>
2026-06-25 14:20:33 +0800

    Update version to 1.3.0 and enhance GenericJSONEncoder to support Enum serialization

--- fun_things/generic_json_encoder.py
+++ fun_things/generic_json_encoder.py
@@ -1,20 +1,23 @@
 from datetime import datetime
+from enum import Enum
 from json import JSONEncoder
 
 try:
     from bson import ObjectId
-except Exception:
 
-    class ObjectId:
-        pass
+except Exception:
+    ObjectId = None
 
 
 class GenericJSONEncoder(JSONEncoder):
     def default(self, o):
-        if isinstance(o, ObjectId):
+        if ObjectId and isinstance(o, ObjectId):
             return str(o)
 
         if isinstance(o, datetime):
             return o.isoformat()
+
+        if isinstance(o, Enum):
+            return o.value
 
         return JSONEncoder.default(self, o)
